7db32fc46b4870601a53b9189f3252dce171d028
Luis rojas <luis.alberto.rojas@bootcamp-upgrade.com>
2024-05-29 20:01:04 +0200

    add getFile|filecontroller, filemode, filerouter

---
+++ Backend/models/file.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const fileSchema = new mongoose.Schema({
+  filename: {
+    type: String,
+    required: true,
+  },
+  meetingData: {
+    meetingId: { type: mongoose.Schema.Types.ObjectId, ref: "Meeting" },
+    rol: [{ type: String, required: true }],
+  },
+});
+
+const File = mongoose.model("File", fileSchema, "files");
+module.exports = File;
