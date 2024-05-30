451895d45cdc9954d36b2860a443e25dcb65f6fe
SergioReportella <145614590+SergioReportella@users.noreply.github.com>
2024-05-30 19:50:10 +0200

    Add getUserMeetings, addUserMeeting, getMeetingUsers, delUserMeeting | meetingController

--- Backend/controllers/meeting.controller.js
+++ Backend/controllers/meeting.controller.js
@@ -44,5 +44,99 @@ const getMeeting = async (req, res, next) => {
                 error: error.message
             })
         }
-
     }
+
+        //Obtener meetings del user
+    const getUserMeetings = async (req, res, next) => {
+        try {
+            const userId = req.body
+            const meetings = await Meeting.find({ meetingUsers: userId })
+            if (meetings) {
+                res.status(200).json({
+                    status: 200,
+                    message: "User Meetings",
+                    meetings: meetings
+                })
+            }
+        }
+        catch (error) {
+            next(error)
+            res.status(500).json({
+                status: 500,
+                message: "Internal Server Error",
+                error: error.message
+            })
+        }
+    }
+
+    //Añadir user al meeting.
+    const addUserMeeting = async (req, res, next) => {
+        try {
+            const { meetingId, userId, roles } = req.body;
+    
+            const meetingToMod = await Meeting.findById(meetingId);
+            if (!meetingToMod) {
+                return res.status(404).json({ message: 'Meeting id not found' });
+            }
+    
+            const userExists = meetingToMod.meetingUsers.some(user => user.userId.toString() === userId);
+            if (!userExists) {
+                meetingToMod.meetingUsers.push({userId,roles});
+            }
+    
+            await meetingToMod.save();
+    
+            return res.status(200).json({ meetingToMod });
+        } catch (error) {
+            return res.status(500).json({ message: 'Error adding user to meeting', error });
+        }
+    }
+
+    //Eliminar user de meeting.
+    const delUserMeeting = async (req, res, next) => {
+        try {
+            const { meetingId, userId } = req.body;
+    
+            const meetingToMod = await Meeting.findById(meetingId);
+            if (!meetingToMod) {
+                return res.status(404).json({ message: 'Meeting id not found' });
+            }
+    
+            meetingToMod.meetingUsers = meetingToMod.meetingUsers.filter(user =>{user.userId.toString() != userId})
+    
+            await meetingToMod.save();
+    
+            return res.status(200).json({ meetingToMod });
+        } catch (error) {
+            return res.status(500).json({ message: 'Error deleting user from meeting', error });
+        }
+    }
+
+    //Obtener users del meetings 
+    const getMeetingUsers = async (req, res, next) => {
+        try {
+            const meetingId = req.params.meetingId;
+            const meeting = await Meeting.findById(meetingId);
+            if (meeting) {
+                res.status(200).json({
+                    status: 200,
+                    message: "Meeting Users",
+                    users: meeting.meetingUsers
+                });
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    message: "Meeting not found"
+                });
+            }
+        } catch (error) {
+            next(error);
+            res.status(500).json({
+                status: 500,
+                message: "Internal Server Error",
+                error: error.message
+            });
+        }
+    }
+
+    module.exports = {getMeeting, getMeetings, getUserMeetings, getMeetingUsers, addUserMeeting , delUserMeeting }
