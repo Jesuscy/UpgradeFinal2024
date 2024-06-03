c72a6f579251a0c8777458c5e9b4442f30b27f9b
Jesuscy <Jesuscarrascoyel.mur@gmail.com>
2024-06-03 20:57:10 +0200

    Fix Import

--- Backend/src/routes/meeting.router.js
+++ Backend/src/routes/meeting.router.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const meetingRouter = express.Router()
 const {getMeeting, getMeetings, createMeeting, addUserMeeting, deleteMeeting, getUserMeetings, getMeetingUsers, editMeeting, delUserMeeting, addRoleToMeetingUser, delRoleFromMeetingUser} = require('../controllers/meeting.controller')
-
+const {getMeetingUsers2} = require('../controllers/meetingUser.controller')
 meetingRouter.get('/meeting',getMeeting)
 meetingRouter.get('/meetings',getMeetings)
 meetingRouter.get('/meetings/userId', getUserMeetings)
-meetingRouter.get('/meetingId/users', getMeetingUsers)
+meetingRouter.post('/meetingId/users', getMeetingUsers)
 meetingRouter.post('/create', createMeeting)
 meetingRouter.post('/add/user', addUserMeeting)
 meetingRouter.post('/delete/user',delUserMeeting)
---
+++ Backend/src/routes/meetingUser.router.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const meetingUserRouter = express.Router()
+const {getMeetingUsers} = require('../controllers/meetingUser.controller')
+
+meetingUserRouter.post('/meetingId/users', getMeetingUsers)
+
+
+
+
+module.exports = meetingUserRouter
