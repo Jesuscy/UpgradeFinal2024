52553a1f19f920796cb52a856fed14789c6c93ad
SergioReportella <145614590+SergioReportella@users.noreply.github.com>
2024-06-07 19:24:59 +0200

    Add renderComponent, Meeting

--- Frontend/FileComite/src/assets/components/Meeting.jsx
+++ Frontend/FileComite/src/assets/components/Meeting.jsx
@@ -3,9 +3,11 @@ import { Header } from "../common/Header";
 import UserRow from '../common/UserRow.jsx';
 import SelectUserRole from '../common/SelectUserRole.jsx';
 import "../../styles/Meeting-styles.css";
+import { FileRows } from "../common/FileRows.jsx";
 
 export const Meeting = (props) => {
     const [showSelectUserRole, setShowSelectUserRole] = useState(false);
+    const [renderOption, setRenderOption] = useState('');
 
     const handleShowSelectUserRole = () => {
         setShowSelectUserRole(true);
@@ -13,6 +15,41 @@ export const Meeting = (props) => {
 
     const handleHideSelectUserRole = () => {
         setShowSelectUserRole(false);
+    };
+
+    const handleSetRenderOption = (option) => {
+        setRenderOption(option);
+    };
+
+    const renderComponent = () => {
+        switch (renderOption) {
+            case 'Role':
+                return <FileRows data={{ option: 'role', meetingName: 'meeting' }} />;
+            case 'Meeting':
+                return <FileRows data={{ option: 'meeting', meetingName: 'meeting'}} />;
+            default:
+                return (
+                    <>
+                        <div className="row box__meetings-title">
+                            <div className="col-md-12 col-sm-12 col-xs-12">
+                                <h1>MEETING TITLE</h1>
+                            </div>
+                        </div>
+                        <div className="row meetings-select">
+                            <div className="col-md-4 col-sm-12 col-xs-12 meetings-select__options">
+                                <h1>Role</h1>
+                                <p>View files corresponding to the role assigned to you</p>
+                                <button className="meetings-select__button" onClick={() => handleSetRenderOption('Role')}>VIEW</button>
+                            </div>
+                            <div className="col-md-4 col-sm-12 col-xs-12 meetings-select__options">
+                                <h1>Meeting</h1>
+                                <p>View all the files from the meeting</p>
+                                <button className="meetings-select__button" onClick={() => handleSetRenderOption('Meeting')}>VIEW</button>
+                            </div>
+                        </div>
+                    </>
+                );
+        }
     };
 
     return (
@@ -28,8 +65,8 @@ export const Meeting = (props) => {
                                     <h1>USERS</h1>
                                 </div>
                                 <div className="user-overflow">
-                                    <div className="col-md-12 col-sm-12 col-xs-12 user-rows"> 
-                                        <UserRow onRoleClick={handleShowSelectUserRole} /> {/* Pasa la función como prop */}                                   
+                                    <div className="col-md-12 col-sm-12 col-xs-12 user-rows">
+                                        <UserRow onRoleClick={handleShowSelectUserRole} />
                                         <UserRow onRoleClick={handleShowSelectUserRole} />
                                         <UserRow onRoleClick={handleShowSelectUserRole} />
                                         <UserRow onRoleClick={handleShowSelectUserRole} />
@@ -40,23 +77,7 @@ export const Meeting = (props) => {
                             </div>
                         </div>
                         <div className="col-md-9 col-sm-12 col-xs-12 meeting-box">
-                            <div className="row box__meetings-title">
-                                <div className="col-md-12 col-sm-12 col-xs-12">
-                                    <h1>MEETING TITLE</h1>
-                                </div>
-                            </div>
-                            <div className="row meetings-select">
-                                <div className="col-md-4 col-sm-12 col-xs-12 meetings-select__options">
-                                    <h1>Role</h1>
-                                    <p>View files corresponding to the role assigned to you</p>
-                                    <button className="meetings-select__button">VIEW</button>
-                                </div>
-                                <div className="col-md-4 col-sm-12 col-xs-12 meetings-select__options">
-                                    <h1>Meeting</h1>
-                                    <p>View all the files from the meeting</p>
-                                    <button className="meetings-select__button">VIEW</button>
-                                </div>
-                            </div>
+                            {renderComponent()}
                         </div>
                     </div>
                 </div>
