c89624263f0a1b1d1655a1c68794389d28ec2d71
Jesuscy <Jesuscarrascoyel.mur@gmail.com>
2024-06-08 18:08:22 +0200

    Add Upload handleFileUpload

--- Frontend/FileComite/src/assets/common/UploadFile.jsx
+++ Frontend/FileComite/src/assets/common/UploadFile.jsx
@@ -1,21 +1,52 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import axios from 'axios'
 import '../../styles/UploadFileStyles.css';
 
 export const UploadFile = (props) => {
 
-    const { toogleShowUpload } = props;
-    const fileInputRef = useRef(null);
+    const { toogleShowUpload } = props
+    const fileInputRef = useRef(null)
+    //UseState para guardar el archivo seleccionado.
+    const [selectedFile, setSelectedFile] = useState(null)
 
     const handleButtonClick = () => {
-        fileInputRef.current.click();
+        fileInputRef.current.click()
     };
 
+    //Selecciono el archivo elegido.
     const handleFileChange = (event) => {
         const file = event.target.files[0];
+        setSelectedFile(file)
         if (file) {
-          console.log(`Selected file: ${file.name}`);
+            console.log(file)
+            console.log(`Selected file: ${file.name}`)
         }
-    };
+    }
+
+    //Hago la petición a subir el archivo.
+    const handleFileUpload = async () => {
+        if (!selectedFile) {
+            alert('First select a file.')
+            return
+        }
+        const formData = new FormData()
+        formData.append('file', selectedFile)
+
+        try {
+            axios.post('http://127.0.0.1:3333/file/upload', formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }})
+
+        }
+        catch(error){
+            
+        }
+
+
+
+    }
 
     return (
         <div className="upload-modal-overlay">
