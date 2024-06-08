8cff641d2f6e01b176d7479373524f9110d945f1
Jesuscy <Jesuscarrascoyel.mur@gmail.com>
2024-06-08 17:22:47 +0200

    Merge

--- Frontend/FileComite/src/assets/common/UploadFile.jsx
+++ Frontend/FileComite/src/assets/common/UploadFile.jsx
@@ -1,39 +1,41 @@
-import React from 'react'
-import '../../styles/UploadFileStyles.css'
+import React, { useRef } from 'react';
+import '../../styles/UploadFileStyles.css';
+
 export const UploadFile = (props) => {
 
-    const toogleShowUpload = props.toogleShowUpload
-    console.log(toogleShowUpload)
-    const selectFileToUpload = () => {
+    const { toogleShowUpload } = props;
+    const fileInputRef = useRef(null);
 
-    }
+    const handleButtonClick = () => {
+        fileInputRef.current.click();
+    };
 
-    const uploadNewFile = () =>{
-
-    }
-
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+          console.log(`Selected file: ${file.name}`);
+        }
+    };
 
     return (
         <div className="upload-modal-overlay">
-        <div className='upload-file'>
-            <div className="row"><h2>UPLOAD NEW FILE</h2></div>
-            <div className="file-name-placeholder">
-                FileName-example
+            <div className="upload-file">
+                <div className="row"><h2>UPLOAD NEW FILE</h2></div>
+                <div className="file-name-placeholder">
+                    FileName-example
+                </div>
+                <div className="row">
+                    <button onClick={handleButtonClick}>Select file</button>
+                    <input
+                        type="file"
+                        style={{ display: 'none' }}
+                        ref={fileInputRef}
+                        onChange={handleFileChange}
+                    />
+                </div>
+                <div className="row"><button>Upload</button></div>
+                <div className="row close"><button onClick={toogleShowUpload}>Close</button></div>
             </div>
-            <div className='row'>
-                <button>select file</button>
-               {  <input
-                    type="file"
-                    style={{ display: 'none' }}  
-                    onChange={uploadNewFile()}
-                /> 
-                }
-            </div>
-            <div className='row'><button>upload</button></div>
-            <div className='row close'><button onClick={toogleShowUpload}>close</button></div>
-
-
         </div>
-        </div>
-    )
-}
+    );
+};
