bf41f10026577f3ae51cc9ace8a72a9b7f56d7a9
Jesuscy <Jesuscarrascoyel.mur@gmail.com>
2024-06-10 17:23:08 +0200

    Meetings Usestate

--- Frontend/FileComite/src/assets/common/MeetingRows.jsx
+++ Frontend/FileComite/src/assets/common/MeetingRows.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../styles/MeetingRowStyles.css'
 import { MeetingRow } from './MeetingRow'
 
 export const MeetingRows = () => {
+    const [meetings, setMeetings] = useState([])
+
+
   return (
     <div className='meeting'>
     <div className="meetings-header">
